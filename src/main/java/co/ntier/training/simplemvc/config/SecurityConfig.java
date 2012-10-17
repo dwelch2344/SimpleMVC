f20d0e25d0a100e8e91cad33db8f4edd235dc567
David Welch <david@davidwelch.co>
2012-10-17 15:46:33 -0600

    Cleaned up security

--- src/main/java/co/ntier/training/simplemvc/config/SecurityConfig.java
+++ src/main/java/co/ntier/training/simplemvc/config/SecurityConfig.java
@@ -8,58 +8,65 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @Configuration
 @ImportResource("/WEB-INF/config/security.xml")
 public class SecurityConfig {
 
-        @Bean(name="org.springframework.security.authenticationManager")
-        public ProviderManager providerManager(){
-                List<AuthenticationProvider> providers = new ArrayList<AuthenticationProvider>();
-                providers.add( new UsernamePasswordAuthenticationProvider("default", "password", "ROLE_DEFAULT"));
-                return new ProviderManager( providers );
-        }
- 
-        
-        public static class UsernamePasswordAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
-            
-            private final String user, password, role;
-            
-            public UsernamePasswordAuthenticationProvider(String user, String password, String role) {
-                    super();
-                    this.user = user;
-                    this.password = password;
-                    this.role = role;
-            }
+	// ***** Stub Code that you should replace! ***********************************
+	
+	private static List<? extends GrantedAuthority> defaultAuthorities = Arrays.asList( new SimpleGrantedAuthority("ROLE_USER") );
+	private static User user = new User("default", BCrypt.hashpw("password", BCrypt.gensalt()), defaultAuthorities);
+	
+	private UserDetailsService userDetailsService = new UserDetailsService() {
+		@Override
+		public UserDetails loadUserByUsername(String username)
+				throws UsernameNotFoundException {
+			if (user.getUsername().equalsIgnoreCase( username )) {
+				return user;
+			}
+			throw new UsernameNotFoundException("Could not find " + username);
+		}
+	};
+	
+	// ************ End Stub code ***************************************
+	
+	@Bean(name = "org.springframework.security.authenticationManager")
+	public ProviderManager providerManager() {
+		List<AuthenticationProvider> providers = new ArrayList<AuthenticationProvider>();
+		providers.add(provider());
+		return new ProviderManager(providers);
+	}
 
-            @Override
-            protected UserDetails retrieveUser(String username,
-                            UsernamePasswordAuthenticationToken auth)
-                            throws AuthenticationException {
-                    
-                    String password = (String) auth.getCredentials();
-                    if( this.user.equalsIgnoreCase(username) && this.password.equals(password)){
-                            return new User( this.user, this.password, Arrays.asList(new SimpleGrantedAuthority( this.role )) );
-                    }
-                    
-                    throw new BadCredentialsException("Invalid account details");
-                    
-            }
-            
-            @Override
-            protected void additionalAuthenticationChecks(UserDetails userDetails,
-                            UsernamePasswordAuthenticationToken authentication)
-                            throws AuthenticationException {
-                    
-            }
+	private DaoAuthenticationProvider provider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setUserDetailsService(userDetailsService);
+		provider.setPasswordEncoder(new BcryptPasswordEncoder());
+		return provider;
+	}
+	
+	private class BcryptPasswordEncoder implements PasswordEncoder {
 
-    }
+		@Override
+		public boolean isPasswordValid(String encPass, String rawPass,
+				Object saltObject) {
+			return BCrypt.checkpw(rawPass, encPass);
+		}
+
+		@Override
+		public String encodePassword(String rawPass, Object salt) {
+			return rawPass;
+		}
+	}
+
 }
