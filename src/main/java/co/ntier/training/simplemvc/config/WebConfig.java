654a18cf3d8c4c66b0036a47ca43272f526611e8
David Welch <david@davidwelch.co>
2012-10-26 22:43:13 -0600

    Forced CGLIB for proxying. Functionally complete

--- src/main/java/co/ntier/training/simplemvc/config/WebConfig.java
+++ src/main/java/co/ntier/training/simplemvc/config/WebConfig.java
@@ -2,6 +2,7 @@ package co.ntier.training.simplemvc.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.WebRequestInterceptor;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
+@EnableAspectJAutoProxy(proxyTargetClass=true)
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
--- src/main/java/co/ntier/training/simplemvc/svc/UserService.java
+++ src/main/java/co/ntier/training/simplemvc/svc/UserService.java
@@ -2,19 +2,71 @@ package co.ntier.training.simplemvc.svc;
 
 import java.util.List;
 
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import co.ntier.training.simplemvc.model.SimpleUser;
+import co.ntier.training.simplemvc.repo.SimpleUserRepository;
 
-public interface UserService extends UserDetailsService{
+@Service
+@Transactional(propagation=Propagation.MANDATORY)
+public class UserService implements UserDetailsService{ //implements IUserService{
+	
+	@PersistenceContext
+	private EntityManager em;
+	
+	@Inject
+	private SimpleUserRepository repo;
+	
+	public SimpleUser create(String name, String email, String password){
+		SimpleUser user = new SimpleUser(name, email, BCrypt.hashpw(password, BCrypt.gensalt()));
+		user = repo.save(user);
+		return user;
+	}
+	
+	public List<SimpleUser> findAll() {
+		List<SimpleUser> users = repo.findAll();
+		return users;
+	}
+	
+	public void simpleUserTest(){
+		SimpleUser user = new SimpleUser("someguy", "email", BCrypt.hashpw("password", BCrypt.gensalt()));
+		em.persist(user);
+		em.flush();
+		List<SimpleUser> users = em.createQuery("From SimpleUser", SimpleUser.class).getResultList();
+		System.out.println(users.size() + " users");
+	}
 
-	SimpleUser create(String name, String email, String password);
+	 
+	/**
+	 * Look up a user by email (which is our "username" in this case)
+	 */
+	@Override
+	// We need to support new transactions here because Spring Security doesn't go through our annotated controller
+	@Transactional(readOnly = true, propagation=Propagation.REQUIRED)
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		List<SimpleUser> results = em.createQuery("from SimpleUser where email=:email", SimpleUser.class)
+				.setParameter("email", email)
+				.getResultList();
+		if( results.isEmpty()){
+			throw new UsernameNotFoundException("Could not locate username: " + email);
+		}
+		
+		Assert.isTrue( results.size() == 1, "Multiple results for username: " + email);
+		
+		return results.get(0);
+	}
 
-	List<SimpleUser> findAll();
-
-	UserDetails loadUserByUsername(String username)
-			throws UsernameNotFoundException;
-
+	
+	
 }
