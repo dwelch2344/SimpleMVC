9c404f6b3df365dce5ba0bb3fd63c99095236c55
David Welch <david@davidwelch.co>
2012-10-29 18:28:07 -0600

    Cleaned up to use plugins from GitHub

--- src/main/java/co/ntier/training/simplemvc/SpringWebAppInitializer.java
+++ src/main/java/co/ntier/training/simplemvc/SpringWebAppInitializer.java
@@ -1,10 +1,17 @@
 package co.ntier.training.simplemvc;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
+import javax.servlet.ServletRegistration.Dynamic;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.SpringServletContainerInitializer;
 import org.springframework.web.WebApplicationInitializer;
@@ -22,7 +29,13 @@ public class SpringWebAppInitializer implements WebApplicationInitializer {
 
 	@Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-
+		Map<String, ? extends ServletRegistration> servlets = servletContext.getServletRegistrations();
+        System.out.println("Servlets: " + servlets.size());
+        for(String key : servlets.keySet()){
+        	ServletRegistration reg = servlets.get(key);
+        	System.out.println(" ==> " + key + " : " + reg.getClassName() + " [" + reg.getMappings() +"]");
+        }
+		
 		// Create the 'root' Spring application context & scans anything for annotations this package
         AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
         root.scan( getClass().getPackage().getName() );
@@ -42,9 +55,28 @@ public class SpringWebAppInitializer implements WebApplicationInitializer {
         appServlet.setLoadOnStartup(1);
         Set<String> mappingConflicts = appServlet.addMapping("/");
         if (!mappingConflicts.isEmpty()) {
-            throw new IllegalStateException("Failed mapping");
+            throw new IllegalStateException("Failed mapping dispatcher to root");
         }
         
+        // Add a debug servlet
+        Dynamic ctx = servletContext.addServlet("test", new TestServlet());
+        Set<String> testMappingConflicts = ctx.addMapping("/test");
+        
+        if (!testMappingConflicts.isEmpty()) {
+            throw new IllegalStateException("Failed mapping test servlet");
+        }
     }
+	
+	@SuppressWarnings("serial")
+	private class TestServlet extends HttpServlet{
+		@Override
+		protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+				throws ServletException, IOException {
+			PrintWriter writer = resp.getWriter();
+			writer.write("Hello World");
+			writer.flush();
+			writer.close();
+		}
+	}
 
 }
--- src/main/webapp/WEB-INF/includes/init.jsp
+++ src/main/webapp/WEB-INF/includes/init.jsp
@@ -1,3 +1,3 @@
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <c:url var="_baseUrl" value="/"/>
-<c:set var="_baseUrl" value="${empty pageContext.request.contextPath ? '' : _baseUrl}"/>
+<c:set var="_baseUrl" value="${empty pageContext.request.contextPath || pageContext.request.contextPath == '/' ? '' : _baseUrl}"/>
